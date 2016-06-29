5731c525078d5224e889815c3fad1b6a74a50675
Manisso <manisso@usa.com>
2016-06-29 17:43:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo "
 
                                                 ";
 echo "";
-echo "[✔] Install.sh will install xshell tool in the system [Y/N] :" ;
+echo "[✔] You will install xshell tool in the system [Y/N] :" ;
 read baba
 if [ $baba == "y" ] ; 
   then
@@ -24,13 +24,13 @@ fi
 echo "[✔] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
 read refdir
 echo "[✔] Checking directories..."
-if [ -d "$refdir/Manisso-Xshell" ] ;
+if [ -d "$refdir/Xshell" ] ;
 then
-echo "[◉] A directory Manisso-Xshell was found! Do you want to replace it? [Y/n]:" ; 
+echo "[◉] A directory Xshell was found! Do you want to replace it? [Y/n]:" ; 
 read mama
 if [ $mama == "y" ] ; 
 then
- rm -R "$refdir/Manisso-Xshell"
+ rm -R "$refdir/Xshell"
 else
  exit
 fi
@@ -38,15 +38,15 @@ fi
 
  echo "[✔] Installing ...";
  echo "";
- git clone https://github.com/Manisso/Xshell.git $refdir/Manisso-Xshell;
+ git clone https://github.com/Manisso/Xshell.git $refdir/Xshell;
  echo "#!/bin/bash 
- perl $refdir/Manisso-Xshell/Xshell.pl" '${1+"$@"}' > xshell;
+ perl $refdir/Xshell/Xshell.pl" '${1+"$@"}' > xshell;
  chmod +x xshell;
  sudo cp xshell /usr/bin/;
  rm xshell;
 
 
-if [ -d "$refdir/Manisso-Xshell" ] ;
+if [ -d "$refdir/Xshell" ] ;
 then
 echo "";
 echo "[✔]Tool istalled with success![✔]";
